9de701e4b0a508a69d9160a55855368d6531e555
roccopaccione <leonardo.rocco.paccione@gmail.com>
2021-03-13 21:21:08 -0600

    feat: added RandomValue method functionality

--- PickRandomCards/CardPicker.cs
+++ PickRandomCards/CardPicker.cs
@@ -3,29 +3,37 @@ namespace PickRandomCards
 {
     public class CardPicker
     {
-        public CardPicker()
-        {
-            static Random random = new Random();
+        static Random random = new Random();
 
-            public static string[] PickSomeCards(int numberOfCards)
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
             {
-                string[] pickedCards = new string[numberOfCards];
-                for (int i = 0; i < numberOfCards; i++)
-                {
-                    pickedCards[i] = RandomValue() + " of " + RandomSuit();
-                }
-                return pickedCards;
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
             }
+            return pickedCards;
         }
+
 
         private static string RandomSuit()
         {
-            throw new NotImplementedException();
+            int value = random.Next(1, 5);
+
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
         }
 
         private static string RandomValue()
         {
-            throw new NotImplementedException();
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
         }
     }
 }
