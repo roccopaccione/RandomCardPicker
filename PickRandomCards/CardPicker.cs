1444ec7a1f8064c998585b6ed9b5e82c09b1ea7d
roccopaccione <leonardo.rocco.paccione@gmail.com>
2021-03-13 21:10:40 -0600

    feat: added picked cards for loop for card gen

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PickRandomCards
+{
+    public class CardPicker
+    {
+        public CardPicker()
+        {
+            static Random random = new Random();
+
+            public static string[] PickSomeCards(int numberOfCards)
+            {
+                string[] pickedCards = new string[numberOfCards];
+                for (int i = 0; i < numberOfCards; i++)
+                {
+                    pickedCards[i] = RandomValue() + " of " + RandomSuit();
+                }
+                return pickedCards;
+            }
+        }
+
+        private static string RandomSuit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string RandomValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
